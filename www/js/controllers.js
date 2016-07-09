aed891cdc8edbcadde52f82546a8dd10878036bd
Salman Mazhar <sa7manmazhar@gmail.com>
2016-07-09 21:27:16 +0200

    style updates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,37 @@
 angular.module('app.controllers', [])
-  
-.controller('nameItCtrl', function($scope) {
 
+.controller('nameItCtrl', function($scope) {
+    $scope.list = [{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'https://media.creativemornings.com/uploads/user/avatar/45150/11-square-avatar.png'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'https://www.xing.com/image/3_b_d_87f40ecdc_19957328_2/omar-al-safi-foto.64x64.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'http://www.pcgamesn.com/sites/default/files/pictures/picture-381-1382089061_0.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'https://www.xing.com/image/3_b_d_87f40ecdc_19957328_2/omar-al-safi-foto.64x64.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'http://www.pcgamesn.com/sites/default/files/pictures/picture-381-1382089061_0.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'https://www.xing.com/image/3_b_d_87f40ecdc_19957328_2/omar-al-safi-foto.64x64.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'http://www.pcgamesn.com/sites/default/files/pictures/picture-381-1382089061_0.jpg'
+    },{
+      title: 'nameit',
+      snippet: 'hello world',
+      img: 'https://www.xing.com/image/3_b_d_87f40ecdc_19957328_2/omar-al-safi-foto.64x64.jpg'
+    }];
 })
- 
