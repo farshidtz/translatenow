faa7a76fdfec20c51d63bc55842ad0989e1db34a
Farshid Tavakolizadeh <tavakoli@fit.fraunhofer.de>
2016-07-10 00:02:53 +0200

    data binding, sequestial queries

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,61 +2,64 @@ angular.module('app.controllers', [])
 
 .controller('nameItCtrl', function($scope) {
 
-  $scope.list = [
-    { name: '', snippet: ''}
-  ];
-
+  $scope.list = [];
   $scope.updateList = function(text){
+
     console.log(text);
     let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+text+"&namespace=0&format=json";
-    $.ajax({
+    $scope.ongoingReq = $.ajax({
       url: url,
       "Content-Type": "application/json; charset=UTF-8",
       "Origin": "http://localhost:8100",
       dataType: "jsonp",
       success: function( result ) {
-        //console.log(result);
-        let list = [];
+        let list = new Array(result[1].length);
+        let pending = result[1].length;
+
         result[1].forEach(function(title, i){
-          let image;
           $.ajax({
             url: "https://en.wikipedia.org/w/api.php?action=query&titles="+title+"&prop=pageimages&format=json&pithumbsize=100",
             "Content-Type": "application/json; charset=UTF-8",
             "Origin": "http://localhost:8100",
             dataType: "jsonp",
             success: function(res) {
-              console.log(res);
+
               $.each( res.query.pages, function( key, value ) {
-                console.log(key, value);
+                let image;
                 if(typeof value.thumbnail != "undefined"){
                   image = value.thumbnail.source;
                 } else {
                   image = "";
                 }
-                console.log(image);
+
+                list[i] = {
+                  title: title,
+                  snippet: result[2][i],
+                  img: image
+                };
+                pending--;
+                if(pending<=0){
+                  $scope.list = list;
+                  $scope.$apply();
+                }
+
+                return false;
               });
 
             }
           });
 
-          list.push(
-            {
-              name: title,
-              snippet: result[2][i],
-              img: image
-            }
-          );
         });
 
-
-        console.log(list);
-        $scope.list = list;
       }
     });
   }
 
-  let onChangeTimeout = 500; // ms
+  let onChangeTimeout = 300; // ms
   $scope.inputChanged = function() {
+    //if(typeof $scope.ongoingReq != "undefined"){
+      //$scope.ongoingReq.abort();
+    //}
     clearTimeout($scope.inputChangedResponse);
     $scope.inputChangedResponse = setTimeout(function(){
       $scope.updateList($scope.textArea);
