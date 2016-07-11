7e29613afc1b2ee5579750949214e53f76149558
Farshid Tavakolizadeh <tavakoli@fit.fraunhofer.de>
2016-07-11 21:11:48 +0200

    programatically hide splash screen

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,10 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide splash screen
+    setTimeout(function() {
+        navigator.splashscreen.hide();
+    }, 100);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
