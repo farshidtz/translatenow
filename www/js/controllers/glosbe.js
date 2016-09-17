dc3f1d05d7c7c3fdc7f078b7c75c3d770f213c60
Farshid Tavakolizadeh <farshid.tz@gmail.com>
2016-09-17 20:45:17 +0200

    glosbe: handling special cases

--- www/js/controllers/glosbe.js
+++ www/js/controllers/glosbe.js
@@ -12,16 +12,24 @@ app.glosbeCtrl = function($scope, $http)
       +(ionic.Platform.isWebView()?"":"&callback=JSON_CALLBACK");
     $http.GetOrJsonp(url, {timeout: $scope.canceler.promise, cache: true}).
     success(function(res, status, headers, config) {
-      if(res.result != "ok" || res.tuc.length==0){
-        $scope.wait.done("getGlosbeTranslation: Bad or no translation");
+      if(res.result != "ok"){
+        $scope.wait.done("getGlosbeTranslation: result not ok");
         return;
+      }
+      if(res.tuc.length==0 || !res.tuc[0].hasOwnProperty('phrase')){
+        $scope.wait.done("getGlosbeTranslation: no translation");
+        return;
+      }
+      var descr = "";
+      if(res.tuc[0].hasOwnProperty('meanings') && res.tuc[0].meanings.length>0){
+        descr = res.tuc[0].meanings[0].text;
       }
 
       // Success
       $scope.list["glosbe:"+text] = {
         rank: -1,
         title: text,
-        descr: res.tuc[0].meanings[0].text,
+        descr: descr,
         img: BingThumb,
         type: 'bing',
         trans: [res.tuc[0].phrase.text]
@@ -29,7 +37,8 @@ app.glosbeCtrl = function($scope, $http)
       $scope.wait.done("getGlosbeTranslation");
     }).
     error(function(data, status, headers, config) {
-      $scope.wait.done("getGlosbeTranslation"); //$scope.showError("bing "+status, data);
+      $scope.wait.done("getGlosbeTranslation");
+      $scope.showError(status, data);
       //$scope.log.push("getBingTranslation:"+status);
       console.warn("glosbe translation error:", status, data);
     });
